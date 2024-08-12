efccdb2fb9c7b107085efda5dedb54d621d58725
Yumna Khan <yumnakhan.usa@outlook.com>
2024-08-12 16:07:30 -0500

    healthsci chatbot UI

--- openai_chatbot/chatbot.py
+++ openai_chatbot/chatbot.py
@@ -10,7 +10,9 @@ load_dotenv()
 api_key = os.getenv("OPEN_API_KEY")
 client = OpenAI(api_key=api_key)
 
-app = Flask(__name__, template_folder="../templates")
+
+app = Flask(__name__, static_folder="../static", template_folder="../templates")
+
 
 # Store conversation state
 conversation = [{"role": "system", "content": "You are a healthcare assistant. Provide accurate and helpful healthcare information and diagnoses based on user symptoms."}]
